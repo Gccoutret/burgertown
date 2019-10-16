0fd2855bf06a69f269c346bc224761fa5823dbe5
GCout <Gccoutret@gmail.com>
2019-10-16 10:03:37 -0500

    mySQL burger_db is set up, package.json is added

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers(
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  burger_name VARCHAR(120),
+  devoured BOOLEAN
+);
+
