8454e0795f67e53b861d792ee626369ebb555d89
GCout <Gccoutret@gmail.com>
2019-10-16 12:48:09 -0500

    added models, and created button within index.handlebars.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,3 @@
+var express = require('express')
+
+// require burgers.js
---
+++ models/burger.js
@@ -0,0 +1,8 @@
+// import 'orm.js
+
+//call the ORM functions using burger specific input for the ORM
+
+
+
+
+//export burger.js
